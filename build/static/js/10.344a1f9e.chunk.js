117938bd06e49791de3f5c18e4d2f6309f6afc2a
saypa95 <saypa_95@mail.ru>
2022-10-19 16:39:16 +0300

    another commit

--- build/static/js/10.344a1f9e.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[10],{492:function(e,s,n){"use strict";n.r(s);n(0);var t=n(5);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
-//# sourceMappingURL=10.344a1f9e.chunk.js.map
